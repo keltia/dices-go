99015119c98e88b2bb0c8460e0055e3f52ef0f6e
Ollivier Robert <roberto@keltia.net>
2015-11-20 23:45:21 +0100

    Add test for bonus form.

--- dice/roll_test.go
+++ dice/roll_test.go
@@ -1,6 +1,8 @@
 package dice
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestParseRoll (t *testing.T) {
 
@@ -19,7 +21,20 @@ func TestParseRoll (t *testing.T) {
 	str = "3d4"
 	res, err = ParseRoll(str)
 	if len(res.Result) != 3 || (res.Sum < 3 || res.Sum > 12) {
-		t.Errorf("Bad string: %v: %d (%d)", err, len(res.Result), res.Sum)
+		t.Errorf("Bad string(%s): %v: %d (%d)", str, err, len(res.Result), res.Sum)
+	}
+
+	str = "D20 +1"
+	res, err = ParseRoll(str)
+	if err != nil {
+		t.Errorf("Error parsing %s: %v", str, err)
+	}
+	if res.Bonus != 1 {
+		t.Errorf("Error applying bonus(%s) %v: %d", str, err, res.Bonus)
+	}
+
+	if len(res.Result) != 1 || (res.Sum < 2 || res.Sum > 21) {
+		t.Errorf("%v: %d (%d)", err, len(res.Result), res.Sum)
 	}
 }
 
