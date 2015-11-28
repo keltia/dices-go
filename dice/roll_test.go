0290a217cc5296602765085ac170692f6827566f
Ollivier Robert <roberto@keltia.net>
2015-11-28 01:20:25 +0100

    More tests.

--- dice/roll_test.go
+++ dice/roll_test.go
@@ -6,31 +6,30 @@ import (
 
 func TestParseRoll (t *testing.T) {
 
+	// valid
 	str := "D20"
+	t.Logf("==> %s", str)
 	res, err := ParseRoll(str)
 	if len(res.Result) != 1 || (res.Sum < 1 || res.Sum > 20) {
 		t.Errorf("Bad string(%s): %v: %d (%d)", str, err, len(res.Result), res.Sum)
 	}
 
 	str = "d20"
+	t.Logf("==> %s", str)
 	res, err = ParseRoll(str)
 	if len(res.Result) != 1 || (res.Sum < 1 || res.Sum > 20) {
 		t.Errorf("Bad string(%s): %v: %d (%d)", str, err, len(res.Result), res.Sum)
 	}
 
-	str = "D2"
-	res, err = ParseRoll(str)
-	if err == nil {
-		t.Errorf("Bad size(%s): %v: %d (%d)", str, err, len(res.Result), res.Sum)
-	}
-
 	str = "3d4"
+	t.Logf("==> %s", str)
 	res, err = ParseRoll(str)
 	if len(res.Result) != 3 || (res.Sum < 3 || res.Sum > 12) {
 		t.Errorf("Bad string(%s): %v: %d (%d)", str, err, len(res.Result), res.Sum)
 	}
 
 	str = "D20 +1"
+	t.Logf("==> %s", str)
 	res, err = ParseRoll(str)
 	if err != nil {
 		t.Errorf("Error parsing %s: %v", str, err)
@@ -41,6 +40,32 @@ func TestParseRoll (t *testing.T) {
 
 	if len(res.Result) != 1 || (res.Sum < 2 || res.Sum > 21) {
 		t.Errorf("%v: %d (%d)", err, len(res.Result), res.Sum)
+	}
+
+	str = "d100"
+	t.Logf("==> %s", str)
+	res, err = ParseRoll(str)
+	if err != nil {
+		t.Errorf("Error, should be valid: %v %v", res, err)
+	} else {
+		if len(res.Result) != 1 || (res.Sum < 1 || res.Sum > 100) {
+			t.Errorf("Bad string(%s): %v: %d (%d)", str, err, len(res.Result), res.Sum)
+		}
+	}
+
+	// invalid
+	str = "D2"
+	t.Logf("==> %s", str)
+	res, err = ParseRoll(str)
+	if err == nil {
+		t.Errorf("Bad size(%s): %v: %d (%d)", str, err, len(res.Result), res.Sum)
+	}
+
+	str = "D101"
+	t.Logf("==> %s", str)
+	res, err = ParseRoll(str)
+	if err == nil {
+		t.Errorf("Bad size(%s): %v", str, err)
 	}
 }
 
