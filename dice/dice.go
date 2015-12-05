37c26440f03f2f30500f3196a9f68de81e433f47
Ollivier Robert <roberto@keltia.net>
2015-12-05 14:17:55 +0100

    Reformat: add documentation, shuffle around.

--- dice/dice.go
+++ dice/dice.go
@@ -25,81 +25,7 @@ var (
 	VALID_DICES = []int{ 4, 6, 8, 10, 12, 20, 30, 100 }
 )
 
-type Dice interface {
-	Roll(r Result) Result
-}
-
-type Dices []Dice
-
-type Result struct {
-	List []int
-	Sum  int
-	Bonus int
-}
-
-type regularDice int
-
-type constantDice int
-
-// API
-
-func NewDices() Dices {
-	return make([]Dice, 0, 10)
-}
-
-func (set Dices) Append(d ...Dice) Dices {
-	return append(set, d...)
-}
-
-func (cd constantDice) Roll(r Result) Result {
-	return r.Append(int(cd))
-}
-
-// AleaJactaEst
-func (d Dices) Roll(r Result) Result {
-    r1 := r
-
-	// Seed the thing
-	mrand.Seed(keySchedule(SEED_SIZE))
-
-    for _,dice := range d {
-        r1 = dice.Roll(r1)
-    }
-    return r1
-}
-
-func (r Result) Append(v int) Result {
-	return Result{append(r.List, v), r.Sum + v, r.Bonus}
-}
-
-func (r Result) BareSum() int {
-	var sum int
-
-	for _, roll := range r.List {
-		sum += roll
-	}
-	return sum
-}
-
-func (nd regularDice) Roll(r Result) Result {
-    return r.Append(internalRoll(int(nd)))
-}
-
-type openDice struct {
-    threshold int
-    d Dice
-}
-
-func (td *openDice) Roll(r Result) Result {
-    if r.Sum != td.threshold {
-        return r
-    }
-    return r.Merge(td.d.Roll(Result{}))
-}
-
-func (r Result) Merge(r1 Result) Result {
-    return Result { append(r.List, r1.List...) , r.Sum+r1.Sum, r.Bonus+r1.Bonus }
-}
+// Private func
 
 // Generate bounded values in a fair way
 // Inspired by @ns_m code on IRC
@@ -117,9 +43,6 @@ func internalRoll(sides int) int {
 		i++
 	}
 }
-
-
-// private func
 
 // check size
 func isValid(size int) bool {
@@ -148,3 +71,92 @@ func keySchedule(seed int) int64 {
 	return acc
 }
 
+// Roll interface (instead of a separate type
+type Dice interface {
+	Roll(r Result) Result
+}
+
+// A set of dices
+type Dices []Dice
+
+// Result of a roll
+type Result struct {
+	List []int
+	Sum  int
+	Bonus int
+}
+
+// variable dice
+type regularDice int
+
+// This is a Dice()
+func (nd regularDice) Roll(r Result) Result {
+    return r.Append(internalRoll(int(nd)))
+}
+
+// Used to represent modifiers like bonus
+type constantDice int
+
+// This is a Dice() too
+func (cd constantDice) Roll(r Result) Result {
+	return r.Append(int(cd))
+}
+
+// Open-ended dices
+type openDice struct {
+    threshold int
+    d Dice
+}
+
+// An openDice is a Dice()
+func (td *openDice) Roll(r Result) Result {
+    if r.Sum != td.threshold {
+        return r
+    }
+    return r.Merge(td.d.Roll(Result{}))
+}
+
+// API
+
+// Make a new set of rolls
+func NewDices() Dices {
+	return make([]Dice, 0, 10)
+}
+
+// That's how we do multiple rolls
+func (set Dices) Append(d ...Dice) Dices {
+	return append(set, d...)
+}
+
+// AleaJactaEst — the actual rolling
+func (d Dices) Roll(r Result) Result {
+    r1 := r
+
+	// Seed the thing
+	mrand.Seed(keySchedule(SEED_SIZE))
+
+    for _,dice := range d {
+        r1 = dice.Roll(r1)
+    }
+    return r1
+}
+
+// Add a constantDice (int) to the roll (i.e. bonus
+func (r Result) Append(v int) Result {
+	return Result{append(r.List, v), r.Sum + v, r.Bonus}
+}
+
+// Sum up all dices w/o bonus
+func (r Result) BareSum() int {
+	var sum int
+
+	for _, roll := range r.List {
+		sum += roll
+	}
+	return sum
+}
+
+// Merge everything incl. bonus
+func (r Result) Merge(r1 Result) Result {
+    return Result { append(r.List, r1.List...) , r.Sum+r1.Sum, r.Bonus+r1.Bonus }
+}
