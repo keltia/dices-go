d9f5ff109b47cce8d4d605cc72ffa4680386ef86
Ollivier Robert <roberto@keltia.net>
2017-06-27 14:15:00 +0200

    Use the assert package.

--- roll_test.go
+++ roll_test.go
@@ -2,6 +2,7 @@ package dice
 
 import (
 	"testing"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestParseRoll (t *testing.T) {
@@ -10,9 +11,8 @@ func TestParseRoll (t *testing.T) {
 	str := "D20"
 	t.Logf("==> %s", str)
 	res, err := ParseRoll(str)
-	if err != nil {
-		t.Errorf("Error parsing %s: %v", str, err)
-	}
+	assert.NoError(t, err, "no error")
+
 	if len(res.List) != 2 || (res.Sum < 1 || res.Sum > 20) {
 		t.Errorf("Bad string(%s): %v: %d (%v)", str, err, len(res.List), res)
 	}
@@ -20,19 +20,26 @@ func TestParseRoll (t *testing.T) {
 	str = "d20"
 	t.Logf("==> %s", str)
 	res, err = ParseRoll(str)
-	if err != nil {
-		t.Errorf("Error parsing %s: %v", str, err)
-	}
+	assert.NoError(t, err, "no error")
+
 	if len(res.List) != 2 || (res.Sum < 1 || res.Sum > 20) {
+		t.Errorf("Bad string(%s): %v: %d (%d)", str, err, len(res.List), res.Sum)
+	}
+
+	str = "d10"
+	t.Logf("==> %s", str)
+	res, err = ParseRoll(str)
+	assert.NoError(t, err, "no error")
+
+	if len(res.List) != 2 || (res.Sum < 1 || res.Sum > 10) {
 		t.Errorf("Bad string(%s): %v: %d (%d)", str, err, len(res.List), res.Sum)
 	}
 
 	str = "3d4"
 	t.Logf("==> %s", str)
 	res, err = ParseRoll(str)
-	if err != nil {
-		t.Errorf("Error parsing %s: %v", str, err)
-	}
+	assert.NoError(t, err, "no error")
+
 	if len(res.List) != 4 || (res.Sum < 3 || res.Sum > 12) {
 		t.Errorf("Bad string(%s): %v: %d (%d)", str, err, len(res.List), res.Sum)
 	}
@@ -40,9 +47,7 @@ func TestParseRoll (t *testing.T) {
 	str = "D20 +1"
 	t.Logf("==> %s", str)
 	res, err = ParseRoll(str)
-	if err != nil {
-		t.Errorf("Error parsing %s: %v", str, err)
-	}
+	assert.NoError(t, err, "no error")
 
 	if len(res.List) != 2 || (res.Sum < 2 || res.Sum > 21) {
 		t.Errorf("%v: %d (%d)", err, len(res.List), res.Sum)
@@ -51,26 +56,20 @@ func TestParseRoll (t *testing.T) {
 	str = "d100"
 	t.Logf("==> %s", str)
 	res, err = ParseRoll(str)
-	if err != nil {
-		t.Errorf("Error, should be valid: %v %v", res, err)
-	} else {
-		if len(res.List) != 2 || (res.Sum < 1 || res.Sum > 100) {
-			t.Errorf("Bad string(%s): %v: %d (%d)", str, err, len(res.List), res.Sum)
-		}
+	assert.NoError(t, err, "no error")
+
+	if len(res.List) != 2 || (res.Sum < 1 || res.Sum > 100) {
+		t.Errorf("Bad string(%s): %v: %d (%d)", str, err, len(res.List), res.Sum)
 	}
 
 	// invalid
 	str = "D2"
 	t.Logf("==> %s", str)
 	res, err = ParseRoll(str)
-	if err == nil {
-		t.Errorf("Bad size(%s): %v: %d (%d)", str, err, len(res.List), res.Sum)
-	}
+	assert.Error(t, err, "no error")
 
 	str = "D101"
 	t.Logf("==> %s", str)
 	res, err = ParseRoll(str)
-	if err == nil {
-		t.Errorf("Bad size(%s): %v", str, err)
-	}
+	assert.Error(t, err, "no error")
 }
