0b616fe98aea98350976cc32b23e5b46727c8487
Ollivier Robert <roberto@keltia.net>
2018-02-16 22:16:05 +0100

    Move commands into their own file.  Move init into setupCLI().

---
+++ cmd/dices-go/cmds.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/abiosoft/ishell"
+	"github.com/keltia/dices-go"
+	"strings"
+)
+
+func roll(c *ishell.Context) {
+	str := strings.Join(c.Args, " ")
+	res, err := dice.ParseRoll(str)
+	if err != nil {
+		c.Printf("Error: %v\n", err)
+	} else {
+		c.Printf("%s = %v (%d)\n", str, res.List, res.Sum)
+		if res.Bonus != 0 {
+			c.Printf(" Bonus was %d\n", res.Bonus)
+		}
+	}
+
+}
+
+func cmdDice(c *ishell.Context) {
+	if len(c.Args) == 0 {
+		c.Printf("error: you must specify something (nn)Ddd( +nn)\n")
+		return
+	}
+	roll(c)
+}
--- cmd/dices-go/dices.go
+++ cmd/dices-go/dices.go
@@ -2,43 +2,13 @@ package main
 
 import (
 	"github.com/abiosoft/ishell"
-	"github.com/keltia/dices-go"
-	"strings"
 )
 
 const (
 	DicesVersion = "1.2.1"
 )
 
-func roll(c *ishell.Context) {
-	str := strings.Join(c.Args, " ")
-	res, err := dice.ParseRoll(str)
-	if err != nil {
-		c.Printf("Error: %v\n", err)
-	} else {
-		c.Printf("%s = %v (%d)\n", str, res.List, res.Sum)
-		if res.Bonus != 0 {
-			c.Printf(" Bonus was %d\n", res.Bonus)
-		}
-	}
-
-}
-
-func cmdDice(c *ishell.Context) {
-	if len(c.Args) == 0 {
-		c.Printf("error: you must specify something (nn)Ddd( +nn)\n")
-		return
-	}
-	roll(c)
-}
-
-func main() {
-
-	shell := ishell.New()
-	shell.Printf("Dices version %s\n", DicesVersion)
-	shell.SetPrompt("Dices> ")
-	shell.SetHistoryPath(".history")
-
+func setupCLI(shell *ishell.Shell) {
 	shell.AddCmd(&ishell.Cmd{
 		Name: "dice",
 		Help: "Roll dices",
@@ -71,5 +41,16 @@ func main() {
 			c.Printf("Version: %s\n", DicesVersion)
 		},
 	})
+}
+
+func main() {
+
+	shell := ishell.New()
+	shell.Printf("Dices version %s\n", DicesVersion)
+	shell.SetPrompt("Dices> ")
+	shell.SetHistoryPath(".history")
+
+	setupCLI(shell)
+
 	shell.Run()
 }
